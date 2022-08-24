f8880c8ebd2fae0c6c71b56b12c89480dddc95d5
DRMAD <drmaddx@gmail.com>
2022-08-23 22:08:03 -0400

    updated game

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,14 +3,20 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <p>
+    <!-- <p> -->
       <!-- {{rando}} -->
 
-      <button>{{ value }}</button>
+      <!-- <button>{{ value }}</button>
 
-    </p>
+    </p> -->
+
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+
   `,
   styles: [
+    'button { width: 100%; height: 100%; font-size: 5em !important;}'
   ]
 })
 export class SquareComponent {
